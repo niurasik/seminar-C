eda48701e891240d704ad972b1b9d5ae98a1fc4b
Anna <tchernovaniura@yandex.ru>
2022-09-02 13:54:07 +0300

    добавили проект

---
+++ Zadacha 2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+ if (num1 > num2){
+     System.Console.WriteLine("Первое число больше второго");
+ }
+ else{
+    System.Console.WriteLine("Первое число меньше второго");
+ } 
+
