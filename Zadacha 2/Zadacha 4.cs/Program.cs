e1c131c2fe2accd4297975fa7697bd00188450ed
Anna <tchernovaniura@yandex.ru>
2022-09-02 14:14:47 +0300

    код для задачи 4

---
+++ Zadacha 2/Zadacha 4.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+ if (a > b && a > c){
+     System.Console.WriteLine("Число а максимальное");
+ }
+ else if (b > c){
+    System.Console.WriteLine("Число b максимальное");
+ }
+ else{
+    System.Console.WriteLine("Число c максимальное");
+ }    
+
