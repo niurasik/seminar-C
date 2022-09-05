85fe6d6c958df0aec35319a4ec09a030013761e0
Anna <tchernovaniura@yandex.ru>
2022-09-05 14:26:46 +0300

    код к задаче 15

---
+++ Zadacha 15/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите цифру дня недели: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 5){
+    System.Console.WriteLine("да");
+}
+else
+    System.Console.WriteLine("нет");
