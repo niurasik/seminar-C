e8b355a27bdacb21f004d7815b4b39d6d4853705
Anna <tchernovaniura@yandex.ru>
2022-09-05 14:16:23 +0300

    код к задаче 10

---
+++ Zadacha 10/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+// insert one 3 symb number
+int n = Convert.ToInt32(Console.ReadLine());
+int result = n /10 % 10;
+System.Console.WriteLine($"{n} -> {result}");
