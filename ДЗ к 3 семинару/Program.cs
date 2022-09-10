8929ac49f14325ecc6c422a0925436826e2b3feb
Anna <tchernovaniura@yandex.ru>
2022-09-10 15:11:37 +0300

    Коды к задачам

---
+++ ДЗ к 3 семинару/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Задача № 23
+// System.Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++){
+//     System.Console.WriteLine(i * i * i);
+// } 
+ 
+
+//  Задача №19
+// System.Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int d1 = n / 10000;
+// n -= d1 * 10000;
+
+// int d2 = n / 1000;
+// n -= d2 * 1000;
+
+// int d3 = n / 100;
+// n -= d3 * 100;
+
+// int d4 = n / 10;
+// n -= d4 * 10;
+
+// int d5 = n % 10;
+
+// if (d1 == d5 && d2 == d4){
+//     System.Console.WriteLine("Палиндром");
+// }
+// else{
+//     System.Console.WriteLine("Не палиндром");
+// }    
+
+
+// Задача №21
+// int x1 = 3;
+// int y1 = 6;
+// int z1 = 8;
+// int x2 = 2;
+// int y2 = 1;
+// int z2 = -7;
+// double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 -y1, 2) + Math.Pow(z2 -z1, 2));
+// System.Console.WriteLine(result);
+
